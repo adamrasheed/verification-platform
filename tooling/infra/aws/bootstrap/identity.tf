bc9d02ce998befb492a93a383b68433d2e845403
Adam <adamrasheed91@gmail.com>
2026-08-06 18:56:45 -0700

    Allow migration endpoint refresh

--- tooling/infra/aws/bootstrap/identity.tf
+++ tooling/infra/aws/bootstrap/identity.tf
@@ -138,6 +138,7 @@ resource "aws_iam_role_policy" "github_deploy" {
         Action = [
           "budgets:DescribeBudget",
           "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeNetworkInterfaces",
           "ec2:DescribePrefixLists",
           "ec2:DescribeRouteTables",
           "ec2:DescribeSecurityGroupRules",
